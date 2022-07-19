ce77504703dde475f953ec75b99519e1d2443008
KsanaSal <oasalivon@gmail.com>
2022-07-19 23:53:25 +0300

    Solution task 2

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,7 @@
-const listCategories = document.querySelectorAll('#categories .item');
-console.log('Number of categories: ' + listCategories.length);
+const listCategories = document.querySelectorAll("#categories .item");
+console.log("Number of categories: " + listCategories.length);
 listCategories.forEach(function (item) {
-    console.log('');
+    console.log("");
     console.log(`Category: ${item.children[0].innerHTML}`);
     console.log(`Elements: ${item.children[1].childElementCount}`);
-})
+});
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,14 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+    "Potatoes",
+    "Mushrooms",
+    "Garlic",
+    "Tomatos",
+    "Herbs",
+    "Condiments",
 ];
+const newLi = ingredients.map((el) => {
+    const createLi = document.createElement("li");
+    createLi.innerText = el;
+    return createLi;
+});
+document.querySelector("#ingredients").append(...newLi);
