4567baf2cf6522396cebefce96318198b05978a2
KsanaSal <oasalivon@gmail.com>
2022-07-21 23:10:56 +0300

    Change task 5

--- js/task-05.js
+++ js/task-05.js
@@ -2,5 +2,7 @@ const input = document.querySelector("#name-input");
 const output = document.querySelector("#name-output");
 input.addEventListener("input", (e) => {
     output.textContent =
-        e.currentTarget.value.length > 0 ? e.currentTarget.value : "Anonymous";
+        e.currentTarget.value.length > 0
+            ? e.currentTarget.value.trim()
+            : "Anonymous";
 });
