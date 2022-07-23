d077fbe1502914e124c398a936e1bc3fe376bf78
KsanaSal <oasalivon@gmail.com>
2022-07-23 23:42:43 +0300

    Solution task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
+const form = document.querySelector(".login-form");
+const handeleSubmit = (e) => {
+    e.preventDefault();
+    const {
+        elements: { email, password },
+    } = e.currentTarget;
 
+    if (email.value === "" || password.value === "") {
+        return alert("Please fill in all the fields!");
+    }
+
+    console.log({ Email: email.value, Password: password.value });
+    e.currentTarget.reset();
+};
+form.addEventListener("submit", handeleSubmit);
